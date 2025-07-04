ae8dcd0a2c8eccd99910551e79db8605388a0945
AlvinQID <github@alvinq.my.id>
2025-07-04 11:08:48 +0700

    Create LibsManager.java

---
+++ src/main/java/id.my.alvinq.prokitid.app/libs/LibsManager.java
@@ -0,0 +1,97 @@
+package id.my.alvinq.prokitid.app.libs;
+
+import dalvik.system.DexClassLoader;
+import android.content.Context;
+import java.io.*;
+import java.lang.reflect.Method;
+import java.util.jar.JarFile;
+import java.util.jar.JarEntry;
+import org.json.JSONObject;
+
+public class LibsManager {
+  private final Context context;
+  private final File cDir;
+  //private final DexClassLoader dcl;
+  public LibsManager(Context ctx) {
+    String dirPath = context.getDir("alvinqid", Context.MODE_PRIVATE).getAbsolutePath();
+    File cDir = new File(dirPath, "cache/dexout");
+    if(!cDir.exists()) cDir.mkdirs();
+    this.cDir = cDir;
+    this.context = context;
+  }
+
+  public static LibsManager get(Context ctx) {
+    LibsManager lm = new LibsManager(ctx);
+    return lm;
+  }
+  public void loadLib(File file) {
+    DexClassLoader dcl = new DexClassLoader(file.getAbsolutePath(),this.cDir.getAbsolutePath(),null,this.context.getClassLoader());
+    String className = getMainClassFromManifest(jarFile);
+    if (className == null) {
+        Logger.get().error("main class tidak ditemukan di manifest.json di jar: " + jarFile.getName());
+        return;
+    }
+    Logger.get().info("Loaded Class -> " + className);
+    try {
+        Class<?> clazz = dcl.loadClass(className);
+        Method method = clazz.getDeclaredMethod("onLoad", Context.class);
+        method.invoke(null, this.context); // static method, no instance
+        Logger.get().info("Loaded Class -> " + className + " Done!");
+    } catch (Exception e) {
+        Throwable real = e instanceof InvocationTargetException ? ((InvocationTargetException) e).getCause() : e;
+        String errorLog = getStackTraceAsString(real);
+        //System.err.println(errorLog); // atau kirim ke LeviLogger
+        Logger.get().error("Error!: " + errorLog);
+    }
+  }
+  public void unLoadLib(File file) {
+    DexClassLoader dcl = new DexClassLoader(file.getAbsolutePath(),this.cDir.getAbsolutePath(),null,this.context.getClassLoader());
+    String className = getMainClassFromManifest(jarFile);
+    if (className == null) {
+        Logger.get().error("main class tidak ditemukan di manifest.json di jar: " + jarFile.getName());
+        return;
+    }
+    Logger.get().info("Unloaded Class -> " + className);
+    try {
+        Class<?> clazz = dcl.loadClass(className);
+        Method method = clazz.getDeclaredMethod("onLoad", Context.class);
+        method.invoke(null, this.context); // static method, no instance
+        Logger.get().info("Unloaded Class -> " + className + " Done!");
+    } catch (Exception e) {
+        Throwable real = e instanceof InvocationTargetException ? ((InvocationTargetException) e).getCause() : e;
+        String errorLog = getStackTraceAsString(real);
+        //System.err.println(errorLog); // atau kirim ke LeviLogger
+        Logger.get().error("Error!: " + errorLog);
+    }
+  }
+  private String getMainClassFromManifest(File jarFile) {
+        try (JarFile jar = new JarFile(jarFile)) {
+            JarEntry entry = jar.getJarEntry("manifest.json");
+            if (entry == null) return null;
+
+            InputStream input = jar.getInputStream(entry);
+            StringBuilder jsonBuilder = new StringBuilder();
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    jsonBuilder.append(line);
+                }
+            }
+
+            JSONObject json = new JSONObject(jsonBuilder.toString());
+            return json.getString("main");
+
+        } catch (Exception e) {
+            Logger.get().error("Error!: " + e.toString());
+        }
+        return null;
+  }
+  public static String getStackTraceAsString(Throwable th) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        th.printStackTrace(pw);
+        return sw.toString();
+  }
+  
+}
