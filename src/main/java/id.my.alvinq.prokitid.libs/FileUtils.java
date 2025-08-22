b0b8e2f55f35b6f087f5910fdb0617d475782341
AlvinQID <github@alvinq.my.id>
2025-08-22 13:10:18 +0700

    Create FileUtils.java

---
+++ src/main/java/id.my.alvinq.prokitid.libs/FileUtils.java
@@ -0,0 +1,16 @@
+package id.my.alvinq.prokitid.libs
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class FileUtils {
+    public static void copyFile(String from, String to) throws IOException {
+        Files.copy(
+            Path.of(from), 
+            Path.of(to), 
+            StandardCopyOption.REPLACE_EXISTING // overwrite jika file sudah ada
+        );
+    }
+}
