aa9ff5db0b97764a50321b0a0d57edafd4e1a45e
AlvinQID <github@alvinq.my.id>
2025-08-22 21:20:44 +0700

    Update Libs.java

--- src/main/java/id.my.alvinq.prokitid.libs/Libs.java
+++ src/main/java/id.my.alvinq.prokitid.libs/Libs.java
@@ -8,7 +8,14 @@ public class Libs {
   public static void onLoad(Context context) {
     CacheLogger.logAllCacheFiles(context);
     Toast.makeText(context, "PL loaded!", Toast.LENGTH_LONG).show();
-    System.load("/storage/emulated/0/alvinqid/mods/libmod.so");
+    File cacheDir = context.getCacheDir();
+    File dataDir = new File(cacheDir, "libs/prokitid");
+    if(!dataDir.exists) dataDir.mkdirs();
+    File mod = new File(dataDir, "libmod.so");
+    if(mod.exists) mod.delete();
+    File em = new File("/storage/emulated/0/alvinqid/mods/libmod.so");
+    FileUtils.copyFile(em.getAbsolutePath(),mod.getAbsolutePath());
+    System.load(mod.getAbsolutePath());
     FileUtils.copyFolder(context.getCacheDir().getAbsolutePath(), "/storage/emulated/0/alvinqid/cache");
   }
 }
