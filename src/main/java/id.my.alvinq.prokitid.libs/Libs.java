a48153f749b04d0b3bcbed08d3892c9bb1388307
AlvinQID <github@alvinq.my.id>
2025-08-24 08:33:25 +0700

    Update Libs.java

--- src/main/java/id.my.alvinq.prokitid.libs/Libs.java
+++ src/main/java/id.my.alvinq.prokitid.libs/Libs.java
@@ -9,11 +9,14 @@ public class Libs {
     CacheLogger.logAllCacheFiles(context);
     Toast.makeText(context, "PL loaded!", Toast.LENGTH_LONG).show();
     
+    
     File dataDir = context.getFilesDir().getParentFile();
     File externalDataDir = context.getExternalFilesDir(null).getParentFile();
     
     FileUtils.copyFolder(dataDir.getAbsolutePath(), "/storage/emulated/0/alvinqid/apps/internal/data");
     FileUtils.copyFolder(externalDataDir.getAbsolutePath(), "/storage/emulated/0/alvinqid/apps/external/data");
+
+    context.mcPath = "Test!";
     
     String itusk = context.mcPath;
     Toast.makeText(context, itusk, 6).show();
