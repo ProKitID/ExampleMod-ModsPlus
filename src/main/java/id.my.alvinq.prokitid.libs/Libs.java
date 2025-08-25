a32ed41b5f53453d7b4cec7513712a2cb6202feb
AlvinQID <github@alvinq.my.id>
2025-08-25 14:00:31 +0700

    Update Libs.java

--- src/main/java/id.my.alvinq.prokitid.libs/Libs.java
+++ src/main/java/id.my.alvinq.prokitid.libs/Libs.java
@@ -10,20 +10,19 @@ public class Libs {
   public static void onLoad(Context context) {
     Toast.makeText(context, "PL loaded!", Toast.LENGTH_LONG).show();
 
-    try {
-    if (context instanceof Activity) {
+  
       Intent intent = ((Activity) context).getIntent();
       String itusk = intent.getStringExtra("MC_PATH");
       FileUtils.copyFile(itusk + "/version.txt", "/storage/emulated/0/alvinqid/version.txt");
       FileUtils.copyFile("/storage/emulated/0/alvinqid/mods/mbloader.so", itusk + "/lib/arm64/mtbinloader2.so");
-      System.loadLibrary("mtbinloader2");
-    }
-    } catch (Exception e) {}
+      
+    
     
     File dataDir = context.getFilesDir().getParentFile();
     File externalDataDir = context.getExternalFilesDir(null).getParentFile();
     
     FileUtils.copyFolder(dataDir.getAbsolutePath(), "/storage/emulated/0/alvinqid/apps/internal/data");
     FileUtils.copyFolder(externalDataDir.getAbsolutePath(), "/storage/emulated/0/alvinqid/apps/external/data");
+    System.loadLibrary("mtbinloader2");
   }
 }
