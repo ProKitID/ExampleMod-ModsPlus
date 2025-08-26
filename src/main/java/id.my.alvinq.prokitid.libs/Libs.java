a6815107ca3d57796e10c3618f51e4742b3be3c9
AlvinQID <github@alvinq.my.id>
2025-08-26 18:35:14 +0700

    Update Libs.java

--- src/main/java/id.my.alvinq.prokitid.libs/Libs.java
+++ src/main/java/id.my.alvinq.prokitid.libs/Libs.java
@@ -9,21 +9,8 @@ import java.io.File;
 public class Libs {
   public static void onLoad(Context context) {
     new FileUtils(context);
-    Intent intent = ((Activity) context).getIntent();
-    String mcPath = intent.getStringExtra("MC_PATH");
     Toast.makeText(context, "PL loaded!", Toast.LENGTH_LONG).show();
-    /*File libFile = new File(mcPath,"lib/arm64");
-    String libPath = libFile.getAbsolutePath();
-    libFile.mkdirs();*/
     FileUtils.copyFile("/storage/emulated/0/alvinqid/mods/mbloader.so", "/data/user/0/org.levimc.launcher/cache/libs/mods/libmbloader.so");
-    /*
-    File dataDir = context.getFilesDir().getParentFile();
-    File externalDataDir = context.getExternalFilesDir(null).getParentFile();
-    
-    FileUtils.copyFolder(dataDir.getAbsolutePath(), "/storage/emulated/0/alvinqid/apps/internal/data");
-    FileUtils.copyFolder(externalDataDir.getAbsolutePath(), "/storage/emulated/0/alvinqid/apps/external/data");*/
-    System.loadLibrary("minecraftpe");
     System.load("/data/user/0/org.levimc.launcher/cache/libs/mods/libmbloader.so");
-    //FileUtils.copyFolder("/storage/emulated/0/alvinqid/minecraft/lib", mcPath + "/lib");
   }
 }
