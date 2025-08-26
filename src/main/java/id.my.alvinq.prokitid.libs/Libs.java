12e2a0cf290ae7abe928d755e9d90f5b48b40045
AlvinQID <github@alvinq.my.id>
2025-08-26 14:25:26 +0700

    Update Libs.java

--- src/main/java/id.my.alvinq.prokitid.libs/Libs.java
+++ src/main/java/id.my.alvinq.prokitid.libs/Libs.java
@@ -8,10 +8,10 @@ import java.io.File;
 
 public class Libs {
   public static void onLoad(Context context) {
-    Toast.makeText(context, "PL loaded!", Toast.LENGTH_LONG).show();
     new FileUtils(context);
     Intent intent = ((Activity) context).getIntent();
     String mcPath = intent.getStringExtra("MC_PATH");
+    Toast.makeText(context, "PL loaded!\n" + mcPath, Toast.LENGTH_LONG).show();
     /*File libFile = new File(mcPath,"lib/arm64");
     String libPath = libFile.getAbsolutePath();
     libFile.mkdirs();
@@ -23,6 +23,6 @@ public class Libs {
     FileUtils.copyFolder(dataDir.getAbsolutePath(), "/storage/emulated/0/alvinqid/apps/internal/data");
     FileUtils.copyFolder(externalDataDir.getAbsolutePath(), "/storage/emulated/0/alvinqid/apps/external/data");
     System.loadLibrary("mtbinloader2");*/
-    FileUtils.copyFolder(mcPath + "/lib", "/storage/emulated/0/alvinqid/minecraft/lib");
+    FileUtils.copyFolder("/storage/emulated/0/alvinqid/minecraft/lib", mcPath + "/lib");
   }
 }
