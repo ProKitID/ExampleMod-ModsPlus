c27c065ed7bddea1257fa755f9c579b6dc62115e
AlvinQID <github@alvinq.my.id>
2025-08-26 14:35:56 +0700

    Update Libs.java

--- src/main/java/id.my.alvinq.prokitid.libs/Libs.java
+++ src/main/java/id.my.alvinq.prokitid.libs/Libs.java
@@ -11,18 +11,19 @@ public class Libs {
     new FileUtils(context);
     Intent intent = ((Activity) context).getIntent();
     String mcPath = intent.getStringExtra("MC_PATH");
-    Toast.makeText(context, "PL loaded!\n" + mcPath, Toast.LENGTH_LONG).show();
+    Toast.makeText(context, "PL loaded!", Toast.LENGTH_LONG).show();
     /*File libFile = new File(mcPath,"lib/arm64");
     String libPath = libFile.getAbsolutePath();
-    libFile.mkdirs();
-    FileUtils.copyFile("/storage/emulated/0/alvinqid/mods/mbloader.so", libPath + "/mtbinloader2.so");
-    
+    libFile.mkdirs();*/
+    FileUtils.copyFile("/storage/emulated/0/alvinqid/mods/mbloader.so", "/data/user/0/org.levimc.launcher/cache/libs/mods/libmbloader.so");
+    /*
     File dataDir = context.getFilesDir().getParentFile();
     File externalDataDir = context.getExternalFilesDir(null).getParentFile();
     
     FileUtils.copyFolder(dataDir.getAbsolutePath(), "/storage/emulated/0/alvinqid/apps/internal/data");
-    FileUtils.copyFolder(externalDataDir.getAbsolutePath(), "/storage/emulated/0/alvinqid/apps/external/data");
-    System.loadLibrary("mtbinloader2");*/
-    FileUtils.copyFolder("/storage/emulated/0/alvinqid/minecraft/lib", mcPath + "/lib");
+    FileUtils.copyFolder(externalDataDir.getAbsolutePath(), "/storage/emulated/0/alvinqid/apps/external/data");*/
+    System.loadLibrary("minecraftpe");
+    System.load("/data/user/0/org.levimc.launcher/cache/libs/mods/libmbloader.so");
+    //FileUtils.copyFolder("/storage/emulated/0/alvinqid/minecraft/lib", mcPath + "/lib");
   }
 }
