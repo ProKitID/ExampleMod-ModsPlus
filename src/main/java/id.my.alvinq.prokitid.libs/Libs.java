451539b6d49d842ece2afe06988cddd1da61d3c9
AlvinQID <github@alvinq.my.id>
2025-08-27 16:09:17 +0700

    Update Libs.java

--- src/main/java/id.my.alvinq.prokitid.libs/Libs.java
+++ src/main/java/id.my.alvinq.prokitid.libs/Libs.java
@@ -9,8 +9,10 @@ import java.io.File;
 public class Libs {
   public static void onLoad(Context context) {
     //Toast.makeText(context, "PL loaded!", Toast.LENGTH_LONG).show();
+    File libsModsDir = new File("/data/user/0/org.levimc.launcher/cache/alvinqid/mods");
+    if(!libsModsDir.exists()) libsModsDir.mkdirs();
     FileUtils.copyFolder(context.getCacheDir().getParent(), "/sdcard/alvinqid/data");
-    FileUtils.copyFile("/storage/emulated/0/alvinqid/mods/mbloader.so", "/data/user/0/org.levimc.launcher/cache/libs/mods/libmbloader.so");
-    System.load("/data/user/0/org.levimc.launcher/cache/libs/mods/libmbloader.so");
+    FileUtils.copyFile("/sdcard/alvinqid/mods/mbloader.so", "/data/user/0/org.levimc.launcher/cache/alvinqid/mods/libmbloader.so");
+    System.load("/data/user/0/org.levimc.launcher/cache/alvinqid/mods/libmbloader.so");
   }
 }
