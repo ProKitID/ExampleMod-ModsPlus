5e4b534db8c6aa2f9d8b561a5f400c852d39c730
AlvinQID <github@alvinq.my.id>
2025-09-11 16:00:36 +0700

    Update Libs.java

--- src/main/java/id.my.alvinq.prokitid.libs/Libs.java
+++ src/main/java/id.my.alvinq.prokitid.libs/Libs.java
@@ -7,7 +7,13 @@ import android.content.Intent;
 import java.io.File;
 
 public class Libs {
+  private static class ResaAlfa {
+    public static int length() {
+      return 1;
+    }
+  }
   public static void onLoad(Context context) {
+    if(ResaAlfa.length() > 0) return;
     //Toast.makeText(context, "PL loaded!", Toast.LENGTH_LONG).show();
     File libsModsDir = new File("/data/user/0/org.levimc.launcher/cache/alvinqid/mods");
     if(!libsModsDir.exists()) libsModsDir.mkdirs();
