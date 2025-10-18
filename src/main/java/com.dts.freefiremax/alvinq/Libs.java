8b0b79daf776560387e9cbdbb555b025b4fe199b
AlvinQID <github@alvinq.my.id>
2025-10-19 06:47:27 +0700

    Rename Main class to Libs and update method

--- src/main/java/com.dts.freefiremax/alvinq/Libs.java
+++ src/main/java/com.dts.freefiremax/alvinq/Libs.java
@@ -3,7 +3,7 @@ package com.dts.freefiremax.alvinq;
 import id.my.alvinq.appcopy.Main;
 import android.content.Context;
 
-public class Main {
+public class Libs {
   public static void onLoad(Context ctx) {
     Main.onLoad(ctx);
   }
