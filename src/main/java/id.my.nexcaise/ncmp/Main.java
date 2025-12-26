a2381566ae505a6415863c8553c859d936389a7f
AlvinQID <github@alvinq.my.id>
2025-12-26 10:24:49 +0700

    Update and rename src/main/java/id.my.alvinq.prokitid.libs/Libs.java to src/main/java/id.my.nexcaise/ncmp/Main.java

---
+++ src/main/java/id.my.nexcaise/ncmp/Main.java
@@ -0,0 +1,9 @@
+package id.my.nexcaise.ncmp;
+
+import android.content.Context;
+
+public class Main {
+  public static void onLoad(Context context) {
+    System.loadLibrary("BetterBrightness");
+  }
+}
